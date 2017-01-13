5c167bf14b18e9d7ced1edd7d5b58cd8904c1fdf
ThiasTux <mathias.ciliberto@gmail.com>
2017-01-13 18:24:23 +0000

    First commit. First skeleton of the app.

---
+++ README.md
@@ -0,0 +1,1 @@
+# BlueSenseHub-Android
---
+++ app/src/main/java/uk/ac/sussex/android/bluesensehub/uicontroller/views/DeviceView.java
@@ -0,0 +1,8 @@
+package uk.ac.sussex.android.bluesensehub.uicontroller.views;
+
+/**
+ * Created by mathias on 13/01/17.
+ */
+
+public class DeviceView  {
+}
