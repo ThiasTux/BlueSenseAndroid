92afaff3a3213cd6723177ab2aea459f82812060
ThiasTux <mathias.ciliberto@gmail.com>
2017-01-13 18:38:07 +0000

    Launcher icon changed.

--- app/src/main/java/uk/ac/sussex/android/bluesensehub/uicontroller/activities/MainActivity.java
+++ app/src/main/java/uk/ac/sussex/android/bluesensehub/uicontroller/activities/MainActivity.java
@@ -1,7 +1,7 @@
 package uk.ac.sussex.android.bluesensehub.uicontroller.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import uk.ac.sussex.android.bluesensehub.R;
 
